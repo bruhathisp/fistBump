e6d24849d456f2a06dad170717682c4ad11f36a3
Bruhathi <91585301+bruhathisp@users.noreply.github.com>
2024-07-29 20:04:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -14,7 +14,7 @@ app.post('/mutateTables', async (req, res) => {
     const response = await axios.post('https://api.glideapp.io/api/function/mutateTables', data, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization: 'Bearer 26531c61-3158-4ff3-90c7-70b91aa829f0',
+        'Authorization': 'Bearer 26531c61-3158-4ff3-90c7-70b91aa829f0',
       },
     });
     res.status(200).json(response.data);
